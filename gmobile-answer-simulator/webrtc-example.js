730e4a0a67af571ca704c0ae08d68b76a8cbc8c3
Tanveer J <77818526+mjan2021@users.noreply.github.com>
2022-09-24 11:57:58 -0400

    iceServer fix for remote access

--- gmobile-answer-simulator/webrtc-example.js
+++ gmobile-answer-simulator/webrtc-example.js
@@ -372,8 +372,8 @@ async function getLocalMediaStreams() {
 getLocalMediaStreams();
 //mikemadethis();
 
-//const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}], 'bundlePolicy': 'max-bundle'};
-const configuration = {'iceServers': [{'urls': 'stun:stun.bluepepper.us:3478'}], 'bundlePolicy': 'max-bundle'};
+const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}], 'bundlePolicy': 'max-bundle'};
+// const configuration = {'iceServers': [{'urls': 'stun:stun.bluepepper.us:3478'}], 'bundlePolicy': 'max-bundle'};
 
 
 
